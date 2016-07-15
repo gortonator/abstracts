6a032691501ecf98ccf0d69179bd0e11b957ecbf
gortonator <gortonator@outlook.com>
2016-07-14 17:07:43 -0700

    clean up unused code

--- nuseattle/edu/Main.java
+++ nuseattle/edu/Main.java
@@ -12,14 +12,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    final static String IN_FILE_NAME = "C:\\abstracts\\ECSA2007.txt";
+    final static String IN_FILE_NAME = "C:\\abstracts\\WICSA2001.txt";
     final static String PATH = "C:\\abstracts\\";
 
-    final static String OUTPUT_FILE_NAME = "C:\\abstracts\\ECSA2007new.txt";
+    final static String OUTPUT_FILE_NAME = "C:\\abstracts\\WICSA2001new.txt";
+
    // final static Charset ENCODING = StandardCharsets.UTF_8;
     final static Charset ENCODING = StandardCharsets.US_ASCII;
 
     public static void main(String[] args) {
+
+        // uncomment these to test a specific file
+        //Conference test = new Conference( IN_FILE_NAME, OUTPUT_FILE_NAME);
+        //test.processAbstracts();
+
+
         FilenameFilter filter  = new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
@@ -37,42 +44,12 @@ public class Main {
             System.out.println(outFile);
             Conference test = new Conference( inputFile, outFile);
             test.processAbstracts();
-        }
+        }/*
+*/
 
 
 
-     /*   try {
-         //   List<String> lines =
-           // readTextFile(FILE_NAME);
-            System.out.println("File read ok");
-            //writeTextFile(lines, OUTPUT_FILE_NAME);
-            //System.out.println("File written - we're done");
-        }catch( IOException ex){
-            System.out.println (ex.toString());
-            System.out.println("IO exception");
-        } */
     }
 
-    //public static List<String> readTextFile(String aFileName) throws IOException {
-    public static void readTextFile(String aFileName) throws IOException {
-        String line = null;
-        FileReader fr = new FileReader(aFileName);
-        BufferedReader br = new BufferedReader(fr);
-        while ((line  = br.readLine())!= null) {
-            if (line.trim().isEmpty()){ System.out.println("EMPTY LINE DUDE"); }
-            System.out.println(line);
-        }
-        br.close();
 
-
-        /*Path path = Paths.get(aFileName);
-        System.out.println("opened");
-        return Files.readAllLines(path, ENCODING);
-        */
-    }
-
-    public static void writeTextFile(List<String> aLines, String aFileName) throws IOException {
-        Path path = Paths.get(aFileName);
-        Files.write(path, aLines, ENCODING);
-    }
 }
